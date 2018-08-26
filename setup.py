73a9e8d05f5f03009d919f93e5dcfd708f215111
Timo Furrer <tuxtimo@gmail.com>
2018-08-26 18:02:43 +0200

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,78 @@
 # -*- coding: utf-8 -*-
 
+import io
 import os
 import sys
-import ast
 import codecs
-from setuptools import setup, find_packages
+from shutil import rmtree
 
-PROJECT_ROOT = os.path.dirname(__file__)
+from setuptools import find_packages, setup, Command
 
 
-class VersionFinder(ast.NodeVisitor):
-    def __init__(self):
-        self.version = None
+here = os.path.abspath(os.path.dirname(__file__))
 
-    def visit_Assign(self, node):
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+required = ["click"]
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
         try:
-            if node.targets[0].id == '__version__':
-                self.version = node.value.s
-        except:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
             pass
 
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-def read_version():
-    """Read version from sure/__init__.py without loading any files"""
-    finder = VersionFinder()
-    path = os.path.join(PROJECT_ROOT, 'w1thermsensor', '__init__.py')
-    with codecs.open(path, 'r', encoding='utf-8') as fp:
-        file_data = fp.read().encode('utf-8')
-        finder.visit(ast.parse(file_data))
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
 
-    print(finder.version)
-    return finder.version
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
 
 
-setup_args=dict(
+# About dict to store version and package info
+about = dict()
+with codecs.open(
+    os.path.join(here, "w1thermsensor", "__version__.py"), "r", encoding="utf-8"
+) as f:
+    exec(f.read(), about)
+
+
+setup_args = dict(
     name="w1thermsensor",
-    version=read_version(),
+    version=about["__version__"],
     license="MIT",
     description="This little pure python module provides a single class to get the temperature of a w1 sensor",
+    long_description=long_description,
     author="Timo Furrer",
     author_email="tuxtimo@gmail.com",
     maintainer="Timo Furrer",
@@ -45,12 +80,31 @@ setup_args=dict(
     platforms=["Linux"],
     url="http://github.com/timofurrer/w1thermsensor",
     download_url="http://github.com/timofurrer/w1thermsensor",
-    packages=find_packages(exclude=['*tests*']),
-    install_requires=["click"],
-    include_package_data=True
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=required,
+    include_package_data=True,
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ),
+    cmdclass={"upload": UploadCommand},
 )
 
 if sys.version_info.major == 3:
-    setup_args["entry_points"] = {"console_scripts": ["w1thermsensor = w1thermsensor.cli:cli"]}
+    setup_args["entry_points"] = {
+        "console_scripts": ["w1thermsensor = w1thermsensor.cli:cli"]
+    }
 
 setup(**setup_args)
--- w1thermsensor/__init__.py
+++ w1thermsensor/__init__.py
@@ -4,12 +4,9 @@
     This module provides a temperature sensor of type w1 therm.
 """
 
+from .__version__ import __version__  # noqa
 from .core import W1ThermSensor  # noqa
 from .errors import NoSensorFoundError  # noqa
 from .errors import SensorNotReadyError  # noqa
 from .errors import UnsupportedUnitError  # noqa
 from .errors import ResetValueError  # noqa
-
-__version__ = "1.0.5"
-__author__ = "Timo Furrer"
-__email__ = "tuxtimo@gmail.com"
