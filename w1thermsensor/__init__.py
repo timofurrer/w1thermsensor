cb8686079f46d9f17cc7c02d501a9f23c9db8555
Timo Furrer <timo.furrer@roche.com>
2020-06-14 18:23:36 +0200

    Dump version to 2.0 pre-release

--- w1thermsensor/__init__.py
+++ w1thermsensor/__init__.py
@@ -4,7 +4,7 @@
 
 __description__ = "A Python package and CLI tool to work with w1 temperature sensors like DS1822, DS18S20 & DS18B20 on the Raspberry Pi, Beagle Bone and other devices."  # noqa
 __license__ = "MIT"
-__version__ = "1.3.0"
+__version__ = "2.0.0a1"
 __author__ = "Timo Furrer"
 __author_email__ = "tuxtimo@gmail.com"
 __url__ = "http://github.com/timofurrer/w1thermsensor"
